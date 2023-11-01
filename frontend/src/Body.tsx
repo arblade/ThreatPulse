75298d7600eff47d70696900993b04099f85abb7
arblade <raphael.arblade@gmail.com>
2023-11-01 18:37:10 +0100

    dev-front

--- frontend/src/Body.tsx
+++ frontend/src/Body.tsx
@@ -6,6 +6,8 @@ export interface Article {
   description: string;
   image_url: string;
   badges: string[];
+  link: string;
+  source: string;
 }
 
 export function Body() {
@@ -21,7 +23,11 @@ export function Body() {
   }, []);
   return (
     <>
-      <div className="mx-16 mt-10 flex flex-col">
+      <div className="mx-10 mt-5    flex flex-col">
+        <div className="text-center text-2xl my-10 font-bold text-gray-800 ">
+          Welcome hunter, your <span className="text-purple-700">feed</span> of
+          the day is here ...
+        </div>
         <div className="flex justify-start items-center flex-col space-y-2">
           <div className="flex flex-col space-y-4">
             {" "}
--- frontend/src/Card.tsx
+++ frontend/src/Card.tsx
@@ -3,21 +3,24 @@ import { Badge } from "@/components/ui/badge";
 export function Card({ article }: { article: Article }) {
   return (
     <>
-      <div className="rounded-lg max-w-[40rem] max-h-[8rem] break-words">
+      <div className="rounded-lg max-w-[40rem] max-h-[9rem] break-words hover:bg-gray-100 py-1 px-1 hover:cursor-pointer rounded-xl">
         <div className="flex space-x-4">
-          <div className="object-fill max-h-5rem max-w-5rem inline-flex ">
+          <div className="object-fill max-h-5rem max-w-5rem inline-flex relative">
             <img
               src={article.image_url}
               className="w-[13rem] h-[8rem] min-w-[13rem] min-h-[8rem] object-cover rounded-xl"
             />
+            <div className="absolute bottom-[0.25rem] right-1">
+              <Badge>{article.source}</Badge>
+            </div>
           </div>
           <div className="">
             {" "}
-            <div className="font-semibold">{article.title}</div>
-            <div className="font-normal text-gray-600 mt-1 max-h-[4.5rem] overflow-clip">
+            <div className="font-semibold mt-1  ">{article.title}</div>
+            <div className="font-normal text-gray-600 mt-1 max-h-[4rem] overflow-clip text-[0.9rem]">
               {article.description}
             </div>
-            <div className="flex space-x-1 mt-1 mb-0.5">
+            <div className="flex space-x-1 mt-1 mb-[0.2rem]">
               {article.badges.map((bg) => (
                 <div className="">
                   {" "}
